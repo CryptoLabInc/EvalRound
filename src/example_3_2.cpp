074beb3a18ccffb8caeb99a8519741f5b57ed62b
ksh1102 <ksh@cryptolab.co.kr>
2022-04-26 22:37:08 +0900

    giving up on making dynamic

--- src/example_3_2.cpp
+++ src/example_3_2.cpp
@@ -1,15 +1,16 @@
 #include "common.h"
 #include "HEAAN/HEAAN.h"
 
-#include <vector>
+#include <iostream>
 
 int main()
 {
-  double *zr = new double(N/2), *zi = new double(N/2);
-  R_Q<LOGQ, 1> pt;
+  double zr[N/2], zi[N/2];
+  R_Q<LOGQ, N> pt;
 
   set_test_message(zr, zi);
   encode(zr, zi, Delta, pt);
+  decode(pt, Delta, zr, zi);
 
   // set input message
   // encode it into pt
@@ -19,6 +20,4 @@ int main()
   // check distribution of pt+qI
   // gen normal distribution
   // check similarity
-
-  delete zr, zi;
 }
