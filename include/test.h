e4d2fd83e21591712382e6d0b85cbd30d9e46e36
ksh1102 <ksh@cryptolab.co.kr>
2022-05-01 07:07:46 +0900

    refine random gens

--- include/test.h
+++ include/test.h
@@ -7,24 +7,27 @@
 #include "HEAAN/DFT.h"
 
 void set_test_message(double zr[N/2], double zi[N/2]) {
+    //sampleUniform(zr, zr+N/2);
+    //sampleUniform(zi, zi+N/2);
+    for(int i = 0; i < N / 2; ++i) {
+        double x = (double) (i) / (N/2) * M_PI;
+        zr[i] = cos(x) / sqrt(2);
+        zi[i] = sin(x) / sqrt(2);
+    }
+}
+
+void set_random_message(double zr[N/2], double zi[N/2]) {
     sampleUniform(zr, zr+N/2);
     sampleUniform(zi, zi+N/2);
-/*    for(int i = 0; i < N / 2; ++i) {
-        double x = (double) (i*i) / (N/2) * M_PI;
-        zr[i] = cos(x);
-        zi[i] = sin(x);
-    }
-    */
 }
 
 void set_test_message(double m[N]) {
-    sampleUniform(m, m+N);
-/*    for(int i = 0; i < N / 2; ++i) {
-        double x = (double) (i*i) / (N/2) * M_PI;
-        zr[i] = cos(x);
-        zi[i] = sin(x);
+    //sampleUniform(m, m+N);
+    for(int i = 0; i < N; ++i) {
+        double x = (double) (i) / N * M_PI;
+        m[i] = cos(x);
     }
-    */
+    
 }
 
 void set_test_rounded_message(double zr[N/2], double zi[N/2]) {
@@ -32,29 +35,21 @@ void set_test_rounded_message(double zr[N/2], double zi[N/2]) {
     double pt[N];
     encode(zr, zi, Delta, pt);
     decode(pt, Delta, zr, zi);
-/*    for(int i = 0; i < N / 2; ++i) {
-        double x = (double) (i*i) / (N/2) * M_PI;
-        zr[i] = cos(x);
-        zi[i] = sin(x);
-    }
-    */
 }
 
 void set_test_matrix(double Ar[K][N/2], double Ai[K][N/2]) {
-    /*for(int k = 0; k < K; ++k) {
-        for(int i = 0; i < N/2; ++i) {
-            double x = (double) (i + k) / (N/2) * M_PI;
-            Ar[k][i] = cos(x);
+    for(int k = 0; k < K; ++k) {
+        //sampleUniform(Ar[k], Ar[k]+N/2);
+        //sampleUniform(Ai[k], Ai[k]+N/2);
+        for(int i = 0; i < N / 2; ++i) {
+            double x = (double) (k*i) / (N/2) * M_PI;
+            Ar[k][i] = cos(x) / sqrt(2);
+            Ai[k][i] = sin(x) / sqrt(2);
         }
     }
+}
 
-    for(int k = 0; k < K; ++k) {
-        for(int i = 0; i < N/2; ++i) {
-            double x = (double) (i + k) / (N/2) * M_PI;
-            Ai[k][i] = sin(x);
-        }
-    }*/
-
+void set_random_matrix(double Ar[K][N/2], double Ai[K][N/2]) {
     for(int k = 0; k < K; ++k) {
         sampleUniform(Ar[k], Ar[k]+N/2);
         sampleUniform(Ai[k], Ai[k]+N/2);
