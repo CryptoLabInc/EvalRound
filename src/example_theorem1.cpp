4a6d14a90817f951c54b38d32135e82bf0c29bf5
ksh1102 <ksh@cryptolab.co.kr>
2022-05-01 07:30:57 +0900

    theorem1 snapshot

--- src/example_theorem1.cpp
+++ src/example_theorem1.cpp
@@ -6,36 +6,45 @@
 int main()
 {
   double zr[N/2], zi[N/2];
-  double Ar[K][N/2], Ai[K][N/2];
-  double Azr[N/2], Azi[N/2];
-
   double pt[N];
-  double pt_Az_raw[N], pt_Az[N];
-  double e_Az[N];
 
-  // get pt_Az_raw
+  // get pt
   set_test_rounded_message(zr, zi);
-  set_test_matrix(Ar, Ai);
-  matrix_vector_product(zr, zi, Ar, Ai, Azr, Azi);
-  encode(Azr, Azi, DeltaSq, pt_Az_raw);
-
-  // get pt_Az
   encode(zr, zi, Delta, pt);
-  matrix_vector_product(pt, Ar, Ai, pt_Az);
+  double expected = sqrt(K * N / 12.0) * norm_pt(pt);
+  std::cout << expected << std::endl;
+  std::cout << std::endl;
 
-  // get e_Az
-  sub_pt(pt_Az_raw, pt_Az, e_Az);
+  for(int r = 0; r < 100; ++r) {
+    double Ar[K][N/2], Ai[K][N/2];
+    double Azr[N/2], Azi[N/2];
+    double pt_Az_raw[N], pt_Az[N];
+    double e_Az[N];
+    
+    encode(zr, zi, Delta, pt);
+    
+    // get pt_Az_raw
+    set_random_matrix(Ar, Ai);
+    matrix_vector_product(zr, zi, Ar, Ai, Azr, Azi);
+    encode(Azr, Azi, DeltaSq, pt_Az_raw);
 
-  // sanity check
-  double er[N/2], ei[N/2];
-  decode(e_Az, DeltaSq, er, ei);
-  print_pt("er (for sanity check)", er);
+    // get pt_Az
+    matrix_vector_product(pt, Ar, Ai, pt_Az);
 
-  double measured = square_sum_pt(e_Az);
-  double expected = K * N * square_sum_pt(pt) / 12.0;
-  double bound = K * expected;
-  std::cout << "Measured : " << measured << std::endl;
-  std::cout << "Expected : " << expected << std::endl;
-  std::cout << "Bound : " << bound << std::endl;
-  std::cout << "Measured / Expected : " << (measured / expected) << std::endl;
+    // get e_Az
+    sub_pt(pt_Az_raw, pt_Az, e_Az);
+
+    // sanity check
+    double er[N/2], ei[N/2];
+    decode(e_Az, DeltaSq, er, ei);
+    //print_pt("er (for sanity check)", er);
+
+    double measured = norm_pt(e_Az);
+    std::cout << measured << std::endl;
+    //double bound = K * expected;
+    //std::cout << "Measured : " << measured << std::endl;
+    //std::cout << "Expected : " << expected << std::endl;
+    //std::cout << "Bound : " << bound << std::endl;
+    //std::cout << "Measured / Expected : " << (measured / expected) << std::endl;
+  } 
 }
