f94775d0a6f94be345e159679cb2f121dd4f8799
ksh1102 <ksh@cryptolab.co.kr>
2022-05-03 14:11:54 +0900

    fix param

--- include/BIG/define.h
+++ include/BIG/define.h
@@ -5,10 +5,8 @@
 
 constexpr int LOGDELTA = 50;
 constexpr std::uint64_t Delta = 1ULL << LOGDELTA;
-constexpr int D = 3; // number of matrices multiplying
-constexpr int LOGQ = 256;
-constexpr int LOGQ_UP = LOGQ * 2;
-constexpr int LOGN = 10;
+constexpr int LOGQ = 512;
+constexpr int LOGN = 16;
 constexpr int N = 1 << LOGN;
-constexpr int h = 64;
 constexpr int K = 16;
+constexpr int D = LOGN -1; // number of matrices multiplying
--- include/SIMPLE/define.h
+++ include/SIMPLE/define.h
@@ -3,10 +3,8 @@
 #include <cstdint>
 #include <cmath>
 
-constexpr int LOGDELTA = 10;
+constexpr int LOGDELTA = 20;
 constexpr std::uint64_t Delta = 1ULL << LOGDELTA;
-constexpr int D = 1; // number of matrices multiplying
-constexpr int LOGN = 10;
+constexpr int LOGN = 16;
 constexpr int N = 1 << LOGN;
-constexpr int h = 64;
 constexpr int K = 16;
