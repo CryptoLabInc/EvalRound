8db462934cd9d6427ea8066a937355ce5cd8d8e5
ksh1102 <ksh@cryptolab.co.kr>
2022-05-09 13:26:46 +0900

    reduce template spec

--- src/experiment/lemma1.cpp
+++ src/experiment/lemma1.cpp
@@ -13,20 +13,20 @@ int main()
   Message <LOGN> z, V;
   SimplePlaintext<LOGN> pt, pt_v_raw, pt_v, e, res;
 
-  set_test_message<LOGN>(z);
-  encode<LOGN>(z, Delta, pt);
+  set_test_message(z);
+  encode(z, Delta, pt);
   double expected = sqrt(N /12.0) * norm(pt);
   std::cout << expected << std::endl;
   std::cout << std::endl;
 
   for(int r = 0; r < 100; ++r) {
-    set_random_message<LOGN>(V);
-    encode_raw<LOGN>(V, Delta, pt_v_raw);
-    encode<LOGN>(V, Delta, pt_v);
-    sub<LOGN>(pt_v_raw, pt_v, e);
-    conv<LOGN>(e, pt, res);
+    set_random_message(V);
+    encode_raw(V, Delta, pt_v_raw);
+    encode(V, Delta, pt_v);
+    sub(pt_v_raw, pt_v, e);
+    conv(e, pt, res);
 
-    double measured = norm<LOGN>(res);
+    double measured = norm(res);
     std::cout << measured << std::endl;
     //std::cout << "Measured : " << measured << std::endl;
     //std::cout << "Expected : " << expected << std::endl;
