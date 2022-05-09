40f205c355f639a0bee9c861a4898c859177e777
ksh1102 <ksh@cryptolab.co.kr>
2022-05-09 13:07:55 +0900

    change src structure

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 echo lemma1
-build/example_lemma1 >> data/log_lemma1
+build/lemma1 >> data/log_lemma1
 echo lemma2
-build/example_lemma2 >> data/log_lemma2
+build/lemma2 >> data/log_lemma2
 echo theorem1
-build/example_theorem1 >> data/log_theorem1
+build/theorem1 >> data/log_theorem1
 echo corollary
-build/example_corollary >> data/log_corollary
+build/corollary >> data/log_corollary
