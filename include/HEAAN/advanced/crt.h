19d98ff66e9451ea17e0aefe1bed93fe4f9f0368
ksh1102 <ksh@cryptolab.co.kr>
2022-05-10 12:57:28 +0900

    simplified crt constructor

--- include/HEAAN/advanced/crt.h
+++ include/HEAAN/advanced/crt.h
@@ -15,21 +15,34 @@ struct CRT {
   Z_Q<64*(L+1)> Q_over_q[L]; // Q/q[i]
   uint64_t inv_mod_Q_over_q[L]; // (Q/q[i])^-1 (mod q[i])
 
-  CRT(const uint64_t q[L],
-  const uint64_t Q[L],
-  const uint64_t inv_mod_Q_over_q[L],
-  const uint64_t Q_over_q[L][L]) {
+  CRT(const uint64_t q[L]) {
     for(int i = 0; i < L; ++i)
       this->q[i] = q[i];
+    
+    // compute Q
+    Q.setzero();
+    Q[0] = 1;
     for(int i = 0; i < L; ++i)
-      this->Q[i] = Q[i];
-    this->Q[L] = 0;
-    for(int i = 0; i < L; ++i)
-      this->inv_mod_Q_over_q[i] = inv_mod_Q_over_q[i];
+      Q *= q[i];
+
+    // compute Q_over_q
     for(int i = 0; i < L; ++i){
-      for(int j = 0; j < L; ++j)
-        this->Q_over_q[i][j] = Q_over_q[i][j];
-      this->Q_over_q[i][L] = 0;
+      Q_over_q[i].setzero();
+      Q_over_q[i][0] = 1;
+      for(int j = 0; j < L; ++j){
+        if(i != j)
+          Q_over_q[i] *= q[j];
+      }
+    }
+    
+    // compute inv_mod_Q
+    for(int i = 0; i < L; ++i) {
+      uint64_t temp = 1;
+      for(int j = 0; j < L; ++j) {
+        if(i != j)
+          temp = mul_mod(temp, q[j], q[i]);
+      }
+      inv_mod_Q_over_q[i] = inv_mod(temp, q[i]);
     }
   }
 
--- include/experiment/big.h
+++ include/experiment/big.h
@@ -7,8 +7,21 @@
 
 constexpr int LOGDELTA = 50;
 constexpr uint64_t Delta = 1ULL << LOGDELTA;
-constexpr int LOGQ = 512;
+constexpr int LOGQ = 512; // max bit of a plaintext slot
 constexpr int LOGN = 5;
 constexpr int N = 1 << LOGN;
 constexpr int K = 16;
 constexpr int D = LOGN -1; // number of matrices multiplying
+
+constexpr int L = 9; // number of RNS primes
+constexpr uint64_t q[L] = { // 9 61bit primes, mod(q[i], 2^17) = 1
+    0xffffffffffc0001ULL,
+    0x10000000006e0001ULL,
+    0xfffffffff840001ULL,
+    0x1000000000860001ULL,
+    0xfffffffff6a0001ULL,
+    0x1000000000980001ULL,
+    0xfffffffff5a0001ULL,
+    0x1000000000b00001ULL,
+    0x1000000000ce0001ULL
+};
