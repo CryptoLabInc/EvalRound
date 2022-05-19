401a64dfaad41f6782ef3ea997d1828367995a59
ksh1102 <ksh@cryptolab.co.kr>
2022-05-19 14:53:59 +0900

    make crt test sign aware

--- src/test/crt_test.cpp
+++ src/test/crt_test.cpp
@@ -10,26 +10,30 @@ int main(){
 
     CRT<L, LOGQ> crt(q);
 
-    uint64_t a_data[L] = {2496847982066394524ULL,6179808641469508490ULL,4613858499374131512ULL,
-14039320798214412288ULL,10083004203411ULL};
+    uint64_t a_data[5] = {2496847982066394524ULL,6179808641469508490ULL,4613858499374131512ULL,
+14039320798214412288ULL, 0ULL};
 
     Z_Q<LOGQ> a;
-    for(int i = 0; i < L; ++i){
+    for(int i = 0; i < 5; ++i){
         a[i]= a_data[i];
     }
 
-    uint64_t a_rns[L] = {753612675309782689ULL,8006693014835917ULL,16048976969132353ULL,376433015983029137ULL,
-148789351586435012ULL};
+    uint64_t a_rns[L] = {171973778066475490ULL,
+    441363031084368163ULL,
+    885382643671141350ULL,
+    1019151257649711462ULL,
+    1052569354770772635ULL};
 
     uint64_t a_rns_measured[L] = {0};
     crt.icrt(a, a_rns_measured);
     for(int i = 0; i < L; ++i)
-        std::cout << a_rns[i] - a_rns_measured[i] << " ";
+        std::cout << a_rns[i] << " " << a_rns_measured[i] << " ";
     std::cout << std::endl;
 
     Z_Q<LOGQ> a_measured;
     crt.crt(a_rns, a_measured);
-    for(int i = 0; i < L; ++i)
+    for(int i = 0; i < 5; ++i) {
         std::cout << a[i] - a_measured[i] << " ";
+    }
     std::cout << std::endl;
 }
